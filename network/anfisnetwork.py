08caf30f26584ba4df65818923002527925d31d9
Zandiir <zandiir.camillov@cuvox.de>
2018-01-05 03:28:40 +0100

    file

--- network/anfisnetwork.py
+++ network/anfisnetwork.py
@@ -16,26 +16,18 @@ class Anfis:
         self.batch_size = 20
         self.params['rules'] = np.zeros(shape=(num_rules, input_dims))
 
-        # self.params['a'] = np.array([[1, 1],
-        #                              [2, 2]])
-        # self.params['b'] = np.array([[2, 2],
-        #                              [3, 1]])
-        # self.params['f'] = np.array([[1, 1],
-        #                              [1, 2]])
-        # self.params['r'] = np.array([0, 0])
+        self.params['a'] = np.array([[1, 3],
+                                     [2, 2]])
+        self.params['b'] = np.array([[2, 1],
+                                     [3, 1]])
+        self.params['f'] = np.array([[1, 1],
+                                     [1, 2]])
+        self.params['r'] = np.array([0, 0])
 
-        self.params['a'] = np.zeros(shape=(input_dims, num_rules))
-        self.params['b'] = np.zeros(shape=(input_dims, num_rules))
-        self.params['f'] = np.zeros(shape=(input_dims, num_rules
-                                           ))
-        self.params['r'] = np.zeros(shape=(num_rules))
-        #
-        #
-        #
-        # for i in range(num_rules):
-        #     for j in range(input_dims):
-        #         self.params['a' + str(j) + str(i)]
-        #         self.params['b' + str(j) + str(i)]
+        # self.params['a'] = np.random.normal(scale=weight_dev, size=(input_dims, num_rules))
+        # self.params['b'] = np.random.normal(scale=weight_dev, size=(input_dims, num_rules))
+        # self.params['f'] = np.random.normal(scale=weight_dev, size=(input_dims, num_rules))
+        # self.params['r'] = np.zeros(shape=(num_rules))
 
         if not hasattr(funcs, self.rule_function):
             raise ValueError('Invalid rule function')
@@ -53,62 +45,66 @@ class Anfis:
         W = np.zeros(shape=(N, self.num_rules))
         o_k = np.zeros(shape=N)
         func_res = np.zeros(shape=(N, self.num_rules))
+        mu_a = np.zeros(shape=(N, self.num_rules))
+        mu_b = np.zeros(shape=(N, self.num_rules))
+        x_less_a = np.zeros(shape=(N, self.num_rules))
+        y_less_a = np.zeros(shape=(N, self.num_rules))
 
+        # forward pass calculation
         for i in range(N):
-            cache_key = 'cache_rule' + str(i)
-            cache_func_key = 'cache_func' + str(i)
-            cache_sum_key = 'w_sum' + str(i)
-            forward_pass_key = "forward_pass_rule" + str(i)
-            w_key = "w_not_norm" + str(i)
+            x_less_a[i] = X[i, 0] - self.params['a'][0]
+            s1 = np.exp(self.params['b'][0] * (X[i, 0] - self.params['a'][0]))
+            mu_a[i] = 1 / (1 + s1)
+            y_less_a[i] = X[i, 1] - self.params['a'][1]
+            s2 = np.exp(self.params['b'][1] * (X[i, 1] - self.params['a'][1]))
+            mu_b[i] = 1 / (1 + s2)
+            # s1 = np.vstack((s1, s2))
+            W[i] = mu_a[i] * mu_b[i]  # s1 * s2 #np.sum(s1, axis=0)
 
-            score = X[i]
-            score, self.cache[cache_key] = forward_pass_rules(score, self.params['a'], self.params['b'])
-            self.cache[forward_pass_key] = score
-            score = np.prod(score, axis=1)  # [w1, w2, w3.....]
-            self.cache[w_key] = score
-            sum = np.sum(score)  # w1 + w2 + w3 + w4
+        # sad je W = shape(N, m)
 
-            score /= sum  # w1'. w2'
-            W[i] = score.reshape(1, score.shape[0])
-            func_res[i], self.cache[cache_func_key] = forward_pass_function(X[i], self.params['f'],
-                                                                            self.params['r'])  # 1x2
-            output = W[i].dot(func_res[i].T)  # 1x1
-            o_k[i] = output
+        row_sum = np.sum(W, axis=1, keepdims=True)
+        row_sum[abs(row_sum - 1e-6) < 1e-6] = 1e-6
+        W_average = W / row_sum
+
+        f = X.dot(self.params['f']) + self.params['r']  # NxM
+        # tezine puta f pa suma po njima da se dobije rezultat
+        z = W_average.dot(f.T)#tu ne NxN
+        output = np.sum(z, axis=0)  # ovo su sada output (N,)
 
         if mode == 'test':
             return output
 
         loss, grads = 0.0, {}
 
-        loss, upstream_gradient = quadratic_loss(y=y, o_k=o_k)
-        grads['f'] = np.zeros(shape=self.params['f'].shape)
-        grads['a'] = np.zeros(shape=self.params['a'].shape)
-        grads['b'] = np.zeros(shape=self.params['b'].shape)
+        loss, upstream_gradient = quadratic_loss(y=y, o_k=output)
 
-        for i in range(N):
-            r_key = "r" + str(i)
-            p_key = "p" + str(i)
-            q_key = "q" + str(i)
-            a_key = "a" + str(i)
-            b_key = "b" + str(i)
-            cache_sum_key = 'w_sum' + str(i)
+        w_sum_squared = np.sum(W, axis=1)
+        dz = upstream_gradient * w_sum_squared
+        dp = upstream_gradient * output * X[:, 0]
+        dq = upstream_gradient * output * X[:, 1]
+        dr = upstream_gradient * output
 
-            forward_pass_key = "forward_pass_rule" + str(i)
+        # izracunat zi - zj
 
-            grads[p_key] = upstream_gradient[i] * (W[i]) * X[i, 0].reshape(-1, 1)
-            grads[q_key] = upstream_gradient[i] * (W[i]) * X[i, 1].reshape(-1, 1)
+        dW = np.zeros(shape=(self.num_rules, N))  # transpniraj na kraju da odgovara klasicnom W
 
-            grads[r_key] = upstream_gradient[i] * W[i]
+        for i in range(self.num_rules):
+            s = 0
+            for j in range(self.num_rules):
+                if i == j: continue
 
-            #izracunat zi - zj
-            zi = func_res[i].reshape(-1, 1)
-            zj = func_res[i]
+                s += (f[:, i] - f[:, j]) * W[:, j]
+            dW[i, :] = s / w_sum_squared
 
-            grad_w = np.sum(W[i] * (zi - zj), axis=0)#nisam siguran za axis jedino
-            grad_w /= (np.sum(W[i]) ** 2)
+        dW = dW.reshape(N, self.num_rules)
 
-            grads[a_key] = upstream_gradient[i] * grad_w * self.params['b'] * self.cache[
-                forward_pass_key] * (1 - self.cache[forward_pass_key])
-            grads[b_key] = upstream_gradient[i] * grad_w * (X[i] - self.params['a']) * self.cache[forward_pass_key] * (1 - self.cache[forward_pass_key])
+        da1 = upstream_gradient * self.params['b'][0] * dW * mu_a * (1 - mu_a) * mu_b
+        db1 = upstream_gradient * dW * (-(x_less_a) * mu_a * (1 - mu_a) * mu_b)
+
+        da2 = upstream_gradient * self.params['b'][1] * dW * mu_b * (1 - mu_b) * mu_a
+        db2 = upstream_gradient * dW * (-(y_less_a) * mu_b * (1 - mu_b) * mu_a)
+
+
 
         return loss, grads
